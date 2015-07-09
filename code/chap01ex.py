367ee6c3757d7765896e542ecc1e14135b92d9f5
kielejocain <joecken@gmail.com>
2015-07-09 09:50:38 -0700

    Chapter 2

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -1,26 +1,28 @@
-"""This file contains code for use with "Think Stats",
-by Allen B. Downey, available from greenteapress.com
-
-Copyright 2014 Allen B. Downey
-License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
-"""
+"""Answers to exercises in Think Stats, started from a copy of nsfg.py."""
 
 from __future__ import print_function
 
+from collections import defaultdict
 import numpy as np
 import sys
 
-import nsfg
 import thinkstats2
 
 
-def main(script):
-    """Tests the functions in this module.
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz'):
+    """Reads the NSFG pregnancy data.
 
-    script: string script name
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
     """
-    print('%s: All tests passed.' % script)
-
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    # CleanFemPreg(df)
+    return df
 
 if __name__ == '__main__':
-    main(*sys.argv)
+    df = ReadFemResp()
+    print(df.pregnum.value_counts().sort_index())
